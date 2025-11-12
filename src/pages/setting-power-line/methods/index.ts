5c4cd3561d31bce2caf6bb9e8d74de30cfdf30f9
wangxu <361558338@qq.com>
2025-11-12 14:44:11 +0800

    fix：修复功率曲线表格查看、编辑功能弹窗报错问题

--- src/pages/setting-power-line/methods/index.ts
+++ src/pages/setting-power-line/methods/index.ts
@@ -130,7 +130,7 @@ export const calItAndStep = (arr: ICurvePiontList[], deviceTypeDifferentKey: any
   const length = arr.length
   const min = length ? arr[0][deviceTypeDifferentKey] : 2
   const max = length ? arr[length - 1][deviceTypeDifferentKey] : 25
-  const step = length > 1 ? arr[1][deviceTypeDifferentKey] - min : arr[0][deviceTypeDifferentKey]
+  const step = length > 1 ? arr[1][deviceTypeDifferentKey] - min : (length === 1 ? arr[0][deviceTypeDifferentKey] : 1)
   // const step = +max - +min + 1 < arr.length ? 0.5 : 1
   return { min, max, step }
 }
